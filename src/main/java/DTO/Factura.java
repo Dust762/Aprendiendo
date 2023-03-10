0fdfe2eb5b1dbb72597e99102131e44f154b881d
GERMAN ALEJANDRO AYAVIRE GODOY <german.ayavire@inacapmail.cl>
2023-03-10 14:13:14 -0300

    Avance 2

--- src/main/java/DTO/Factura.java
+++ src/main/java/DTO/Factura.java
@@ -1,6 +1,10 @@
 package DTO;
 
 import DAO.IFactura;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Factura implements IFactura{
     private int id = 0, contadorFac = 0;
@@ -8,17 +12,25 @@ public class Factura implements IFactura{
     private String nombreCliente;
     private int total = 0;
     
-    public Factura(Cliente c){
-        this.id = ++contadorFac;
-        this.nombreCliente = c.getNombre() + " " + c.getApellido();
-        
-    }
-
+  
     @Override
     public Factura generarFactura(Cliente c) {
-        Factura f = new Factura(c);
-        
-        
-        return f;
+        this.nombreCliente = c.getNombre() + " " + c.getApellido();
+        int contador = 0;
+        for (int i = 0; i < c.getProductos().size(); i++) {
+            System.out.println("Productos:");
+            System.out.println("ID: " + c.getProductos().get(i).getIdProd());
+            System.out.println("Nombre: " + c.getProductos().get(i).getNombre());
+            System.out.println("Precio: " + c.getProductos().get(i).getPrecio());
+        }
+        Set<Producto> setTmp = new HashSet<Producto>(c.getProductos());
+        for (Producto producto : setTmp) {
+            System.out.println(producto.toString() + " \n");
+        }
+        return null;
+    }
+    
+    private boolean revisarExistencia(List<Producto> lista, Producto p){
+        return true;
     }
 }
